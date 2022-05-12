2d869f9ecde003459a6c713fa31a6b28e971ddce
lelouch187 <vanushs187@gmail.com>
2022-05-12 17:06:10 +0600

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return  (<header className='header'>
+        <img src='https://steamuserimages-a.akamaihd.net/ugc/1834649355085352665/6B31DF15A0D2849D271C5176C611C5D7AE5DAEFE/?imw=512&amp;imh=512&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true'/>
+    </header>)
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+  return  (<div className='content'>
+      <div>
+          <img src='https://avatars.mds.yandex.net/i?id=a22abf1a733adb8e9544b5208db52f5c-5233241-images-thumbs&n=13'/>
+      </div>
+      <div>
+          ava + description
+      </div>
+      <div>
+          My posts
+          <div>
+              New post
+          </div>
+          <div>
+              <div>
+                  post 1
+              </div>
+              <div>
+                  post 2
+              </div>
+          </div>
+      </div>
+  </div>)
+}
+
+export default Profile;
